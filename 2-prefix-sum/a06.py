4f9ea3ec28a3f9c70e968799c3f2e0e09a71938d
kanade-o <sotaohnuma@gmail.com>
2026-03-03 20:21:56 +0900

    基本の累積和

---
+++ 2-prefix-sum/a06.py
@@ -0,0 +1,13 @@
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+L = [0] * Q
+R = [0] * Q
+for j in range(Q):
+    L[j], R[j] = map(int, input().split())
+
+S = [0] * (N + 1)
+for i in range(N):
+    S[i + 1] = S[i] + A[i]
+
+for j in range(Q):
+    print(S[R[j]] - S[L[j] - 1])
