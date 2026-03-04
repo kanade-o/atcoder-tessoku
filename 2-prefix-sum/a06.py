452a5b7529387acfa80f864752417be23ff48a17
kanade-o <sotaohnuma@gmail.com>
2026-03-04 10:44:49 +0900

    復習

--- 2-prefix-sum/a06.py
+++ 2-prefix-sum/a06.py
@@ -1,13 +1,12 @@
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
-L = [0] * Q
-R = [0] * Q
-for j in range(Q):
-    L[j], R[j] = map(int, input().split())
+L, R = [0] * Q, [0] * Q
+for q_i in range(Q):
+    L[q_i], R[q_i] = map(int, input().split())
 
-S = [0] * (N + 1)
-for i in range(N):
-    S[i + 1] = S[i] + A[i]
+S = [0] * (N + 1)  # S[0] = 0
+for s_i in range(N):
+    S[s_i + 1] = S[s_i] + A[s_i]
 
-for j in range(Q):
-    print(S[R[j]] - S[L[j] - 1])
+for q_i in range(Q):
+    print(S[R[q_i]] - S[L[q_i] - 1])
